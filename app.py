f07ba4f4e53d9d53e567760358737bbe8676356c
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-15 14:38:28 +0530

    update

--- app.py
+++ app.py
@@ -1,62 +1,62 @@
 import streamlit as st
-import requests
 import time
+from models.knowledge_ai import get_answer
 
 st.title("TrinoCaps AI")
 
 # Chat memory
+
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+ st.session_state.messages = []
 
 st.header("Ask about Medicaps university rules...")
 
 # Display previous chat messages
+
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+ with st.chat_message(message["role"]):
+  st.markdown(message["content"])
 
 # Chat input
+
 question = st.chat_input("Ask about university rules")
 
 if question:
 
-    # show user message
-    st.session_state.messages.append({"role": "user", "content": question})
-    with st.chat_message("user"):
-        st.markdown(question)
 
-    # typing effect
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        message_placeholder.markdown("AI is thinking...")
+# show user message
+ st.session_state.messages.append({"role": "user", "content": question})
 
-        try:
-            response = requests.get(
-                "http://127.0.0.1:8000/ask",
-                params={"question": question}
-            )
+with st.chat_message("user"):
+    st.markdown(question)
 
-            answer = response.json()["answer"]
+# AI response
+with st.chat_message("assistant"):
+    message_placeholder = st.empty()
+    message_placeholder.markdown("AI is thinking...")
 
-            time.sleep(1)
+    time.sleep(1)
 
-            message_placeholder.markdown(answer)
+    answer = get_answer(question)
 
-            st.session_state.messages.append(
-                {"role": "assistant", "content": answer}
-            )
+    message_placeholder.markdown(answer)
 
-        except:
-            message_placeholder.markdown(
-                "Backend server is not running."
-            )
+    st.session_state.messages.append(
+        {"role": "assistant", "content": answer}
+    )
+
 
 # Clear chat button
-if st.button("Clear Chat"):
-    st.session_state.messages = []
-    st.rerun()
 
-st.markdown("""
+if st.button("Clear Chat"):
+ st.session_state.messages = []
+ st.rerun()
+
+# Footer
+
+st.markdown(
+"""
+
 <style>
 .corner-credit {
 position: fixed;
@@ -72,4 +72,6 @@ z-index: 100;
 <div class="corner-credit">
 TrinoCaps AI • Built by Vinit | Powered by Trinovous
 </div>
-""", unsafe_allow_html=True)
+""",
+unsafe_allow_html=True
+)
